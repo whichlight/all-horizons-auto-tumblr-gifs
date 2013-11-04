8a9fa7486b87f61a74605205447e4e06bde6729a
Kawandeep Virdee <whichlight@gmail.com>
2013-11-04 01:37:28 -0500

    using abs paths so it works as a cron

--- make_gifs.sh
+++ make_gifs.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 FILENAME=$1;
-convert `ls tmp_img | grep out.png | sort -n | sed 's/^/tmp_img\//' | tr '\n' ' '` -loop 0 $FILENAME;
+convert `ls $DIR/tmp_img | grep out.png | sort -n | sed "s;^;$DIR/tmp_img/;" | tr '\n' ' ';` -loop 0 $FILENAME;
